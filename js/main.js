11f5a752a4acde51a8366fa494828e0c66a4747a
TonyVyron <antoniofuentesjara@gmail.com>
2024-08-18 14:36:06 -0600

    CV subido

--- js/main.js
+++ js/main.js
@@ -59,16 +59,16 @@
     
     // Typed Initiate
     if ($('.hero .hero-text h2').length == 1) {
-        var typed_strings = $('.hero .hero-text .typed-text').text();
+        var typed_strings = $('.hero .hero-text .typed-text').text().split(', ');
         var typed = new Typed('.hero .hero-text h2', {
-            strings: typed_strings.split(', '),
+            strings: typed_strings,
             typeSpeed: 100,
             backSpeed: 20,
             smartBackspace: false,
             loop: true
         });
     }
-    
+
     
     // Skills
     $('.skills').waypoint(function () {
