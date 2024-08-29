aa253c4dbe80342956a9277ba785415fb89b79aa
TonyVyron <antoniofuentesjara@gmail.com>
2024-08-29 14:10:42 -0600

    fix theme color

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function ($) {
     "use strict";
     
-    // loader
+    // Loader
     var loader = function () {
         setTimeout(function () {
             if ($('#loader').length > 0) {
@@ -11,12 +11,31 @@
     };
     loader();
     
-    
-    // Initiate the wowjs
+    // Mantener la preferencia del usuario al recargar la página
+    window.addEventListener('load', function() {
+        const theme = localStorage.getItem('theme');
+        const themeIcon = document.getElementById('theme-toggle').querySelector('i');
+
+        if (theme === 'light') {
+            document.body.classList.add('light-mode');
+            themeIcon.classList.remove('fa-sun');
+            themeIcon.classList.add('fa-moon');
+        }
+
+        // Verificar si el tema se ha cargado correctamente
+        if (!document.body.classList.contains('light-mode') && theme === 'light') {
+            // Si el tema no se carga correctamente, mostrar nuevamente el loader
+            $('#loader').addClass('show');
+        } else {
+            // Ocultar el loader si el tema se cargó correctamente
+            $('#loader').removeClass('show');
+        }
+    });
+
+    // Iniciar WOW.js
     new WOW().init();
     
-    
-    // Back to top button
+    // Botón de volver arriba
     $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
             $('.back-to-top').fadeIn('slow');
@@ -28,8 +47,7 @@
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
-    
-    
+
     // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 0) {
@@ -38,8 +56,7 @@
             $('.navbar').removeClass('nav-sticky');
         }
     });
-    
-    
+
     // Smooth scrolling on the navbar links
     $(".navbar-nav a").on('click', function (event) {
         if (this.hash !== "") {
@@ -55,9 +72,8 @@
             }
         }
     });
-    
-    
-    // Typed Initiate
+
+    // Typed.js Initiate
     if ($('.hero .hero-text h2').length == 1) {
         var typed_strings = $('.hero .hero-text .typed-text').text().split(', ');
         var typed = new Typed('.hero .hero-text h2', {
@@ -69,14 +85,12 @@
         });
     }
 
-    
     // Skills
     $('.skills').waypoint(function () {
         $('.progress .progress-bar').each(function () {
             $(this).css("width", $(this).attr("aria-valuenow") + '%');
         });
     }, {offset: '80%'});
-
 
     // Testimonials carousel
     $(".testimonials-carousel").owlCarousel({
@@ -85,45 +99,13 @@
         dots: true,
         loop: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             }
         }
     });
-    
-    function copyWhatsAppNumber() {
-        const phoneNumber = "9931074765"; // Reemplaza con tu número de WhatsApp
-        navigator.clipboard.writeText(phoneNumber).then(() => {
-            alert("Número de WhatsApp copiado al portapapeles: " + phoneNumber);
-        }).catch(err => {
-            console.error("Error al copiar el número de WhatsApp: ", err);
-        });
-    }
-    
-    function copyEmail() {
-        const email = "tony@example.com"; // Reemplaza con tu dirección de correo electrónico
-        navigator.clipboard.writeText(email).then(() => {
-            alert("Dirección de correo copiada al portapapeles: " + email);
-        }).catch(err => {
-            console.error("Error al copiar la dirección de correo: ", err);
-        });
-    }
-    
-    document.addEventListener('DOMContentLoaded', function() {
-        var navbarNav = document.querySelector('.navbar-nav');
-        var navbarToggler = document.querySelector('.navbar-toggler');
-        
-        navbarNav.addEventListener('click', function() {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                var bsCollapse = new bootstrap.Collapse(document.getElementById('navbarCollapse'), {
-                    toggle: false
-                });
-                bsCollapse.hide();
-            }
-        });
-    });
-    
-    // Portfolio filter
+
+    // Portafolio filtro
     var portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
         layoutMode: 'fitRows'
@@ -132,15 +114,36 @@
     $('#portfolio-filter li').on('click', function () {
         $("#portfolio-filter li").removeClass('filter-active');
         $(this).addClass('filter-active');
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        portfolioIsotope.isotope({ filter: $(this).data('filter') });
     });
-    
+
+    // Copiar número de WhatsApp
+    function copyWhatsAppNumber() {
+        const phoneNumber = "9931074765"; // Reemplaza con tu número de WhatsApp
+        navigator.clipboard.writeText(phoneNumber).then(() => {
+            alert("Número de WhatsApp copiado al portapapeles: " + phoneNumber);
+        }).catch(err => {
+            console.error("Error al copiar el número de WhatsApp: ", err);
+        });
+    }
+
+    // Copiar correo electrónico
+    function copyEmail() {
+        const email = "tony@example.com"; // Reemplaza con tu dirección de correo electrónico
+        navigator.clipboard.writeText(email).then(() => {
+            alert("Dirección de correo copiada al portapapeles: " + email);
+        }).catch(err => {
+            console.error("Error al copiar la dirección de correo: ", err);
+        });
+    }
+
+    // Cambiar tema
     document.getElementById('theme-toggle').addEventListener('click', function() {
         document.body.classList.toggle('light-mode');
         
         // Cambia el ícono dependiendo del tema
         const themeIcon = this.querySelector('i');
-        if(document.body.classList.contains('light-mode')) {
+        if (document.body.classList.contains('light-mode')) {
             localStorage.setItem('theme', 'light');
             themeIcon.classList.remove('fa-sun');
             themeIcon.classList.add('fa-moon');
@@ -150,19 +153,5 @@
             themeIcon.classList.add('fa-sun');
         }
     });
-    
-    // Mantener la preferencia del usuario al recargar la página
-    window.addEventListener('load', function() {
-        const theme = localStorage.getItem('theme');
-        const themeIcon = document.getElementById('theme-toggle').querySelector('i');
-        if (theme === 'light') {
-            document.body.classList.add('light-mode');
-            themeIcon.classList.remove('fa-sun');
-            themeIcon.classList.add('fa-moon');
-        }
-    });
-    
-    
-    
-})(jQuery);
 
+})(jQuery);
